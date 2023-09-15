b3c2f13a034a8a8ec15337757cc600ef42ec5557
Rustam Aliiev <r.aliiev@mobidev.biz>
2023-09-15 15:12:08 +0300

    lesson4

--- app/src/main/java/com/example/myshoppinglist/data/ShopListRepositoryImpl.kt
+++ app/src/main/java/com/example/myshoppinglist/data/ShopListRepositoryImpl.kt
@@ -7,6 +7,14 @@ object ShopListRepositoryImpl : ShopListRepository {
 
     private val shopList = mutableListOf<ShopItem>()
     private var autoIncrementId = 0
+
+    init {
+        for (i in 0 until 10) {
+            val item = ShopItem("Name $i", i, true)
+            addShopItem(item)
+        }
+    }
+
     override fun addShopItem(item: ShopItem) {
         if (item.id == ShopItem.UNDEFINED_ID) {
             item.id = autoIncrementId++

a3505694bc25d714a8b9f71adf055501ec85901d
Rustam Aliiev <r.aliiev@mobidev.biz>
2023-09-21 16:55:28 +0300

    lesson14_start

---
+++ app/src/main/java/com/example/myshoppinglist/presentation/ShopItemActivity.kt
@@ -0,0 +1,13 @@
+package com.example.myshoppinglist.presentation
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.myshoppinglist.R
+
+class ShopItemActivity: AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_shop_item)
+    }
+}

d768d6fc3093b24dbc323c5d73fa086be7945073
Rustam Aliiev <rustam.miko@gmail.com>
2024-11-13 17:16:23 +0100

    lesson05

--- app/src/main/java/com/example/myshoppinglist/data/ShopListRepositoryImpl.kt
+++ app/src/main/java/com/example/myshoppinglist/data/ShopListRepositoryImpl.kt
@@ -1,11 +1,14 @@
 package com.example.myshoppinglist.data
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import com.example.myshoppinglist.domain.ShopItem
 import com.example.myshoppinglist.domain.ShopListRepository
 
 object ShopListRepositoryImpl : ShopListRepository {
 
     private val shopList = mutableListOf<ShopItem>()
+    private val shopListLD = MutableLiveData<List<ShopItem>>()
     private var autoIncrementId = 0
 
     init {
@@ -20,10 +23,12 @@ object ShopListRepositoryImpl : ShopListRepository {
             item.id = autoIncrementId++
         }
         shopList.add(item)
+        updateList()
     }
 
     override fun deleteShopItem(item: ShopItem) {
         shopList.remove(item)
+        updateList()
     }
 
     override fun editShopItem(item: ShopItem) {
@@ -38,7 +43,11 @@ object ShopListRepositoryImpl : ShopListRepository {
         } ?: throw RuntimeException("Element with id $itemId not found")
     }
 
-    override fun getShopList(): List<ShopItem> {
-        return shopList.toList()
+    override fun getShopList(): LiveData<List<ShopItem>> {
+        return shopListLD
+    }
+
+    private fun updateList() {
+        shopListLD.value = shopList.toList()
     }
 }
--- app/src/main/java/com/example/myshoppinglist/domain/ShopListRepository.kt
+++ app/src/main/java/com/example/myshoppinglist/domain/ShopListRepository.kt
@@ -1,10 +1,12 @@
 package com.example.myshoppinglist.domain
 
+import androidx.lifecycle.LiveData
+
 interface ShopListRepository {
 
     fun addShopItem(item: ShopItem)
     fun deleteShopItem(item: ShopItem)
     fun editShopItem(item: ShopItem)
     fun getShopItem(itemId: Int): ShopItem
-    fun getShopList(): List<ShopItem>
+    fun getShopList(): LiveData<List<ShopItem>>
 }

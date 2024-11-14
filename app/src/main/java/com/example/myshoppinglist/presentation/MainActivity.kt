85440ec8e20ca6fd00e56fed48d31bdaacf8471b
Rustam Aliiev <rustam.miko@gmail.com>
2024-11-14 11:24:59 +0100

    lesson06

--- app/src/main/java/com/example/myshoppinglist/presentation/MainActivity.kt
+++ app/src/main/java/com/example/myshoppinglist/presentation/MainActivity.kt
@@ -1,22 +1,49 @@
 package com.example.myshoppinglist.presentation
 
 import android.os.Bundle
-import android.util.Log
+import android.widget.LinearLayout
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
 import com.example.myshoppinglist.R
+import com.example.myshoppinglist.domain.ShopItem
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var viewModel: MainViewModel
+    private lateinit var llShopList: LinearLayout
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        llShopList = findViewById(R.id.ll_shop_list)
         viewModel = ViewModelProvider(this)[MainViewModel::class.java]
-
         viewModel.shopList.observe(this) {
-            Log.d("MainActivityTest", it.toString())
+            showList(it)
+        }
+    }
+
+    private fun showList(list: List<ShopItem>) {
+        llShopList.removeAllViews()
+        for (shopItem in list) {
+            val layoutId = if (shopItem.enabled) {
+                R.layout.item_shop_enabled
+            } else {
+                R.layout.item_shop_disabled
+            }
+            val view = layoutInflater.inflate(layoutId, llShopList, false)
+            view.findViewById<TextView>(R.id.tv_name).apply {
+                text = shopItem.name
+            }
+            view.findViewById<TextView>(R.id.tv_count).apply {
+                text = shopItem.count.toString()
+            }
+
+            view.setOnLongClickListener {
+                viewModel.changeEnableState(shopItem)
+                true
+            }
+            llShopList.addView(view)
         }
     }
 }

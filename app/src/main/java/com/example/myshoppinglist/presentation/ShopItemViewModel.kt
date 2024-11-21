f5da66d6a8dc6d5c3f6dbcd12fc35ddb507ff0d4
Rustam Aliiev <rustam.miko@gmail.com>
2024-11-21 11:32:13 +0100

    lesson14_ShopItemViewModel

---
+++ app/src/main/java/com/example/myshoppinglist/presentation/ShopItemViewModel.kt
@@ -0,0 +1,61 @@
+package com.example.myshoppinglist.presentation
+
+import androidx.lifecycle.ViewModel
+import com.example.myshoppinglist.data.ShopListRepositoryImpl
+import com.example.myshoppinglist.domain.AddShopItemUseCase
+import com.example.myshoppinglist.domain.EditShopItemUseCase
+import com.example.myshoppinglist.domain.GetShopItemUseCase
+import com.example.myshoppinglist.domain.ShopItem
+
+class ShopItemViewModel : ViewModel() {
+
+    private val repository = ShopListRepositoryImpl
+
+    private val getShopItemUseCase = GetShopItemUseCase(repository)
+    private val addShopItemUseCase = AddShopItemUseCase(repository)
+    private val editShopItemUseCase = EditShopItemUseCase(repository)
+
+    fun getShopItem(itemId: Int): ShopItem {
+        return getShopItemUseCase.getShopItem(itemId)
+    }
+
+    fun addShopItem(inputName: String?, inputCount: String?) {
+        val name = parseName(inputName)
+        val count = parseCount(inputCount)
+        val fieldsValid = validateInput(name, count)
+        if (fieldsValid) {
+            val shopItem = ShopItem(name, count, true)
+            addShopItemUseCase.addShopItem(shopItem)
+        }
+    }
+
+    fun editShopItem(inputName: String?, inputCount: String?) {
+        val name = parseName(inputName)
+        val count = parseCount(inputCount)
+        val fieldsValid = validateInput(name, count)
+        if (fieldsValid) {
+            val shopItem = ShopItem(name, count, true)
+            editShopItemUseCase.editShopItem(shopItem)
+        }
+    }
+
+    private fun parseName(inputName: String?): String {
+        return inputName?.trim() ?: ""
+    }
+
+    private fun parseCount(inputCount: String?): Int {
+        return inputCount?.trim()?.toIntOrNull() ?: 0
+    }
+
+    private fun validateInput(name: String, count: Int): Boolean {
+        var result = true
+        if (name.isEmpty()) {
+            //TODO: show error input name
+            result = false
+        } else if (count <= 0) {
+            //TODO: show error input cont
+            result = false
+        }
+        return result
+    }
+}

a95111a2b6c7c64a4e7a50aaa960891ea3c9b541
Rustam Aliiev <rustam.miko@gmail.com>
2025-01-06 07:14:13 +0100

    lesson26: DB creation

---
+++ app/src/main/java/com/example/myshoppinglist/data/AppDatabase.kt
@@ -0,0 +1,32 @@
+package com.example.myshoppinglist.data
+
+import android.app.Application
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+@Database(entities = [ShopItemDbModel::class], version = 1, exportSchema = false)
+abstract class AppDatabase : RoomDatabase() {
+
+    abstract fun shopListDao(): ShopListDao
+
+    companion object {
+        private var INSTANCE: AppDatabase? = null
+        private val LOCK = Any()
+        private const val DB_NAME = "shop_item.db"
+
+        fun getInstance(context: Application): AppDatabase {
+            INSTANCE?.let {
+                return it
+            }
+            synchronized(LOCK) {
+                INSTANCE?.let {
+                    return it
+                }
+                val db = Room.databaseBuilder(context, AppDatabase::class.java, DB_NAME).build()
+                INSTANCE = db
+                return db
+            }
+        }
+    }
+}

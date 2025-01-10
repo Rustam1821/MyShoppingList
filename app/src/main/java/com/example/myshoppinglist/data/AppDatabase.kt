3e26242efa75f4c67d9a1b13f69e01d23267d87a
Rustam Aliiev <rustam.miko@gmail.com>
2025-01-10 11:28:53 +0100

    lesson31: ViewModelScope

--- app/src/main/java/com/example/myshoppinglist/data/AppDatabase.kt
+++ app/src/main/java/com/example/myshoppinglist/data/AppDatabase.kt
@@ -23,9 +23,7 @@ abstract class AppDatabase : RoomDatabase() {
                 INSTANCE?.let {
                     return it
                 }
-                val db = Room.databaseBuilder(context, AppDatabase::class.java, DB_NAME)
-                    .allowMainThreadQueries()
-                    .build()
+                val db = Room.databaseBuilder(context, AppDatabase::class.java, DB_NAME).build()
                 INSTANCE = db
                 return db
             }
